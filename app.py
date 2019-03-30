ea8611704fd4248cbced37b92bcc944700746844
JonasKopka <jonas.kopka@gmail.com>
2019-03-30 19:59:43 +0100

    Merge

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def student():
-   return render_template('student.html')
+   #return render_template('student.html')
+   return render_template('temp.html')
 
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      print(result)
-      #return render_template("result.html",result = result)
-      return render_template('index.html')
+@app.route('/time', methods = ['POST', 'GET'])
+def time():
+    return render_template('temp2.html')
+#@app.route('/result',methods = ['POST', 'GET'])
+#def result():
+#   if request.method == 'POST':
+#      result = request.form
+#      print(result)
+#      #return render_template("result.html",result = result)
+ #     return render_template('index.html')
 
 @app.route('/recipe', methods=['POST', 'GET'])
 def recipe():
@@ -25,6 +29,8 @@ def recipes(page_id):
 @app.route('/rate', methods=['POST', 'GET'])
 def rate():
     return render_template('rate.html')
+
+
 @app.route('/thanks', methods=['POST', 'GET'])
 def thanks():
     print(request.form['rate'])
